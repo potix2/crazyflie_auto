cad69b780f60bc5585e6db8efb5dd8fbff895151
Katsunori Kanda <potix2@gmail.com>
2016-12-01 22:08:29 +0900

    Add tf publisher

--- scripts/tracker.py
+++ scripts/tracker.py
@@ -17,10 +17,22 @@ class CrazyflieTracker:
 
     def __init__(self):
         rospy.init_node('crazyflie_tracker')
+        self.pub_tf = tf.TransformBroadcaster()
         self.bridge = CvBridge()
 
         # publish transform rate at 50Hz
         self.rate = rospy.Rate(50.0)
+
+        self.target_u = 0
+        self.target_v = 0
+        self.target_d = 0
+
+        self.last_d = 0
+
+
+        self.r = -1.5708
+        self.y = -3.1415
+        self.p = 0
 
         # subscribe to kinect image messages
         rospy.wait_for_message("/kinect2/qhd/camera_info", CameraInfo)
@@ -28,6 +40,7 @@ class CrazyflieTracker:
         # Subscribe to Kinect v2 sd camera_info to get image frame height and width
         rospy.Subscriber('/kinect2/qhd/camera_info', CameraInfo, self.camera_data, queue_size=1)
         rospy.Subscriber("/kinect2/qhd/image_color_rect", Image, self.image_callback, queue_size=1)
+        rospy.Subscriber("/kinect2/qhd/image_depth_rect", Image, self.depth_callback, queue_size=1)
 
         self.rate.sleep()
 
@@ -80,19 +93,53 @@ class CrazyflieTracker:
 
            # Using moments find the center of the object and draw a red outline around the object
            target_m = cv2.moments(target_image)
-           #self.target_u = int(target_m['m10']/target_m['m00'])
-           #self.target_v = int(target_m['m01']/target_m['m00'])
+           self.target_u = int(target_m['m10']/target_m['m00'])
+           self.target_v = int(target_m['m01']/target_m['m00'])
            points = cv2.minAreaRect(target_image)
            box = cv2.boxPoints(points)
            box = np.int0(box)
            cv2.drawContours(image, [box], 0, (0, 0, 255), 2)
-           #rospy.loginfo("Center of target is x at %d and y at %d", int(self.target_u), int(self.target_v))
+           rospy.loginfo("Center of target is x at %d and y at %d", int(self.target_u), int(self.target_v))
 
-           #self.target_found = True               # set flag for depth_callback processing
+           self.target_found = True               # set flag for depth_callback processing
 
            # show image with target outlined with a red rectangle
            cv2.imshow ("Target", image)
            cv2.waitKey(3)
+
+    def depth_callback(self, msg):
+
+        # create OpenCV depth image using default passthrough encoding
+        try:
+            depth_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+        except CvBridgeError as e:
+            print(e)
+
+        # using box (u, v) position, find depth value of Crazyflie point and divide by 1000
+        # to change millimeters into meters (for Kinect sensors only)
+        self.target_d = depth_image[self.target_v, self.target_u] / 1000.0
+        rospy.loginfo("Depth: x at %d  y at %d  z at %f", int(self.target_u), int(self.target_v), self.target_d)
+
+        if self.target_d == 0:
+            self.target_d = self.last_d
+        else:
+            self.last_d  = self.target_d
+
+        # publish Crazyflie tf transform
+        self.update_cf_transform(self.target_u, self.target_v, self.target_d)
+
+    def update_cf_transform(self, x, y, z):
+
+        # send position as transform from the parent "kinect2_ir_optical_frame" to the
+        # child "crazyflie/base_link" (described by crazyflie.urdf.xacro)"
+        self.pub_tf.sendTransform(( x,
+                                    y,
+                                    z),
+                                tf.transformations.quaternion_from_euler(self.r, self.p, self.y),
+                                rospy.Time.now(),
+                                "crazyflie/base_link", "kinect2_ir_optical_frame")
+
+        rospy.loginfo("Send CF transform %f %f %f", x, y, z)
 
 def main(args):
     tracker = CrazyflieTracker()
