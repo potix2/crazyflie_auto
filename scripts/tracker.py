323889feec2ee16a10c1095d7230a95ce67a233f
Katsunori Kanda <potix2@gmail.com>
2017-04-20 21:14:51 +0900

    Extract class to detect an object from a image

--- scripts/tracker.py
+++ scripts/tracker.py
@@ -10,12 +10,13 @@ from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 import tf
 import traceback
+from detector import Detector
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import CameraInfo
 
 
-class CrazyflieTracker:
+class CrazyflieTracker(object):
 
     def __init__(self):
         rospy.init_node('crazyflie_tracker')
@@ -23,6 +24,7 @@ class CrazyflieTracker:
         self.countNotFound = 0
         self.pub_tf = tf.TransformBroadcaster()
         self.bridge = CvBridge()
+        self.detector = Detector()
 
         # publish transform rate at 50Hz
         self.rate = rospy.Rate(50.0)
@@ -83,87 +85,11 @@ class CrazyflieTracker:
         except CvBridgeError as e:
             print(e)
 
-
-        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-        # duck
-        # lower=[ 17  36 229], upper=[ 72 146 255]
-
-
-        h_margin = 10
-        s_margin = 30
-        v_margin = 30
-        # green origami
-        lower = np.array([143 / 2 - h_margin, 63 * 255 / 100 - s_margin, 47 * 255 / 100 - v_margin], np.uint8)
-        upper = np.array([143 / 2 + h_margin, 63 * 255 / 100 + s_margin, 47 * 255 / 100 + v_margin], np.uint8)
-
-        # blue light
-        # lower=[ 88 130 212], upper=[128 210 255]
-        #lower = np.array([88, 130, 229], np.uint8)
-        #upper = np.array([128, 210, 255], np.uint8)
-        mask = cv2.inRange(hsv, lower, upper)
-
-        # dilate and erode with kernel size 11x11
-        cv2.morphologyEx(mask, cv2.MORPH_CLOSE, np.ones((11,11)))
-
-        # find all of the contours in the mask image
-        cImage, contours, heirarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        self.contourLength  = len(contours)
-
-        # Check for at least one target found
-        if self.contourLength < 1:
-            return
-        else:
-            # target found
-            ## Loop through all of the contours, and get their areas
-            try:
-                area = np.array([0.0]*len(contours))
-                for i in range(self.contourLength):
-                   area[i] = cv2.contourArea(contours[i])
-
-                #### Target #### the largest "pink" object
-                # target_image = contours[area.index(max(area))]
-                indicies = np.argpartition(-area, 1)[:1]
-                tu = 0
-                tv = 0
-                for i in indicies:
-                    target_image = contours[i]
-
-                    # Using moments find the center of the object and draw a red outline around the object
-                    target_m = cv2.moments(target_image)
-                    target_u = int(target_m['m10']/target_m['m00'])
-                    target_v = int(target_m['m01']/target_m['m00'])
-                    points = cv2.minAreaRect(target_image)
-                    box = cv2.boxPoints(points)
-                    box = np.int0(box)
-                    cv2.drawContours(image, [box], 0, (0, 0, 255), 2)
-                    # rospy.loginfo("target is x at %d and y at %d", int(target_u), int(target_v))
-                    tu += target_u
-                    tv += target_v
-                self.target_u = int(tu / 2)
-                self.target_v = int(tv / 2)
-
-                # rospy.loginfo("Center of target is x at %d and y at %d", int(self.target_u), int(self.target_v))
-                self.target_found = True               # set flag for depth_callback processing
-                self.countNotFound = 0
-                self.counter += 1
-
-                # show image with target outlined with a red rectangle
-                #cv2.imshow ("Target", image)
-                if self.counter % 5 == 0:
-                    cv2.imwrite("/home/potix2/sampling/%d.png" % self.counter, image)
-
-
-                # TODO: save image when pressed 's' key
-                #cv2.waitKey(3)
-            except:
-                self.countNotFound += 1
-                if self.countNotFound > 10:
-                    self.target_found = False
-                #traceback.print_exc()
-                #rospy.loginfo("failed to detect target: " + traceback.format_tb(e))
-                return
-
+        position = self.detector.detect(image)
+        self.target_found = self.detector.target_found
+        if position is not None:
+            self.target_u = position[0]
+            self.target_v = position[1]
 
     def depth_callback(self, msg):
 
